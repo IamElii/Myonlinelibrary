1e50f51cb7cbe832e193926098a8702b0fbceda6
IamElii <ej.minter28@gmail.com>
2017-12-05 18:43:50 -0500

    Add files via upload

---
+++ OnlineLibrary/inc/data.php
@@ -0,0 +1,136 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+	"title" => "The Hunger Games",
+	"img" => "img/media/hungergames.jpg",
+    "genre" => "Adventure Fiction",
+    "format" => "Print",
+    "year" => 2008,
+    "category" => "Books",
+    "authors" => [
+        "Suzanne Collins",
+
+    ],
+    "publisher" => "Scholastic Press",
+    "isbn" => '978-0-439-02352-8'
+];
+$catalog[102] = [
+    "title" => "The Giver",
+    "img" => "img/media/giver.jpg",
+    "genre" => "Science Fiction",
+    "format" => "Paperback",
+    "year" => 1993,
+    "category" => "Books",
+    "authors" => [
+        "Lois Lowry"
+    ],
+    "publisher" => "Houghton Mifflin",
+    "isbn" => '0-553-57133-8'
+];
+$catalog[103] = [
+    "title" => "Monster",
+    "img" => "img/media/monster.jpg",
+    "genre" => "Mystery",
+    "format" => "Hardcover",
+    "year" => 1999,
+    "category" => "Books",
+    "authors" => [
+        "Walter Dean Myers"
+    ],
+    "publisher" => "Harpercollins",
+    "isbn" => '0-06-440731-4'
+];
+
+//Movies
+$catalog[201] = [
+    "title" => "Deadpool",
+    "img" => "img/media/deadpool.jpg",
+    "genre" => "Action",
+    "format" => "DVD",
+    "year" => 2016,
+    "category" => "Movies",
+    "director" => "Tim Miller",
+    "writers" => [
+        "Rhett Reese",
+				"Paul Wernick"
+
+    ],
+    "stars" => [
+        "Ryan Reynolds",
+				"Morena Baccarin",
+				"Ed Skrein",
+				"T. J. Miller",
+				"Gina Carano",
+
+];
+$catalog[202] = [
+    "title" => "Creed",
+    "img" => "img/media/creed.jpg",
+    "genre" => "Sports",
+    "format" => "Blu-ray",
+    "year" => 2015,
+    "category" => "Movies",
+    "director" => "Ryan Coogler",
+    "writers" => [
+        "Ryan Coogler"
+    ],
+    "stars" => [
+        "Michael B. Jordan",
+				"Sylvester Stallone",
+				"Tessa Thompson",
+				"Phylicia Rashad",
+				"Anthony Bellew",
+
+];
+$catalog[203] = [
+    "title" => "Get Out",
+    "img" => "img/media/getout.jpg",
+    "genre" => "Thriller",
+    "format" => "Blu-ray",
+    "year" => 2017,
+    "category" => "Movies",
+    "director" => "Jordan Peele",
+    "writers" => [
+        "Jordan Peele",
+
+    ],
+    "stars" => [
+					"Daniel Kaluuya",
+					"Allison Williams",
+					"Bradley Whitford",
+					"Caleb Landry Jones",
+					"Stephen Root",
+					"Lakeith Stanfield",
+					"Catherine Keener",
+
+];
+
+//Music
+$catalog[301] = [
+    "title" => "January 28TH",
+    "img" => "img/media/jan28.jpg",
+    "genre" => "Hip-hop/ Rap",
+    "format" => "Digital",
+    "year" => 2014,
+    "category" => "Music",
+    "artist" => "J. Cole"
+];
+$catalog[302] = [
+    "title" => "Hood Politics",
+    "img" => "img/media/kdot.jpg",
+    "genre" => "Hip-hop/Rap",
+    "format" => "Digital",
+    "year" => 2015,
+    "category" => "Music",
+    "artist" => "Kendrick Lamar"
+];
+$catalog[303] = [
+    "title" => "Day 'N' Night",
+    "img" => "img/media/cudi.jpg",
+    "genre" => "Hip-hop/ Rap",
+    "format" => "Digital",
+    "year" => 2008,
+    "category" => "Music",
+    "artist" => "Kid Cudi"
+];
